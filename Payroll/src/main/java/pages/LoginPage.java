b23fc0a7544aebd20d494bdc07494615f135b352
sazika <sazika@Aadit>
2020-06-16 10:14:17 +0530

    testing,,,,,,,

--- Payroll/src/main/java/pages/LoginPage.java
+++ Payroll/src/main/java/pages/LoginPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 WebDriver driver;
-
+//test dadssd
 	
 	@FindBy(xpath="//input[@id='loginform-username']")
 	WebElement usertxt;
